db3e0c554e58ff77a1b7bad53d741fdf08d75f94
Eley Ng <eley_ng@utexas.edu>
2018-11-19 02:42:17 -0800

    added chunking and tried to build training set; code for chunking breaks for abnormal datasets

---
+++ startup.m
@@ -0,0 +1,5 @@
+addpath('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN')
+addpath('/home/eleyng/Documents/subl/learning/cs229project')
+addpath('/home/eleyng/Documents/subl/learning')
+addpath('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN/10_19')
+load('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN/10_19/pawned.mat')
