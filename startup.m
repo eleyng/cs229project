3f145abc59b5493aabe72b163cc0ed3c6d156168
Eley Ng <eley_ng@utexas.edu>
2018-11-19 02:58:01 -0800

    removed startup

--- startup.m
+++
@@ -1,5 +0,0 @@
-addpath('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN')
-addpath('/home/eleyng/Documents/subl/learning/cs229project')
-addpath('/home/eleyng/Documents/subl/learning')
-addpath('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN/10_19')
-load('/media/eleyng/TOSHIBA EXT/Learning/PAWN/PAWN/10_19/pawned.mat')
